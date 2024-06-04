6c2b78ad4ce9017a9a0586f94a31599f8b40e1c8
Prizren89 <BeratKastrati1989@gmail.com>
2024-06-04 21:57:37 +0200

    endboss added

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,11 +1,17 @@
-class Cloud extends MovableObject{
-y = 20;
-width = 500;
-height = 300; 
+class Cloud extends MovableObject {
+    y = 20;
+    width = 500;
+    height = 300;
     
-    
+
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = 0 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        this.moveLeft();
     }
 }
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,18 +4,43 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
+    otherDirection = false;
 
-    loadImage(path){
+
+    loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
 
     moveRight() {
         console.log('moving-right');
     }
 
-    moveLeft(){
+
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+    }
+
+
+    playAnimation() {
+
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
 
     }
 }
